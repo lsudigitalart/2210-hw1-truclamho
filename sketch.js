8649b8da9f356cfda843f2c63c1216622b373a19
Truc Lam Ho <tho12@lsu.edu>
2024-08-28 14:57:00 -0500

    Assigment 1 shapes added.

---
+++ sketch.js
@@ -0,0 +1,93 @@
+function setup() {
+    createCanvas(700,windowHeight);
+    background(186, 186, 186);
+    noStroke();
+
+    fill(237, 241, 242);
+    stroke(150);
+    ellipse(500, 700, 800, 800);
+    ellipse(500, 700, 700, 700);
+    ellipse(500, 700, 600, 600);
+    ellipse(500, 700, 500, 500);
+    ellipse(500, 700, 400, 400);
+    ellipse(500, 700, 300, 300);
+
+    
+    fill(0);
+    stroke(150);
+    ellipse(50, 650, 200, 600);
+    ellipse(50, 650, 150, 550);
+    ellipse(50, 650, 100, 500);
+    ellipse(50, 650, 50, 450);
+
+
+    //circle four
+    stroke(150);
+    fill(0);
+    ellipse(450, 240, 350, 350);
+    ellipse(450, 240, 300, 300);
+    ellipse(450, 240, 250, 250);
+    ellipse(450, 240, 200, 200);
+
+    ellipse(550, 240, 250, 250);
+    ellipse(550, 240, 200, 200);
+    ellipse(550, 240, 150, 150);
+    ellipse(550, 240, 100, 100);
+    ellipse(550, 240, 50, 50);
+
+    ellipse(450, 240, 150, 150);
+    
+    //circle one
+    noStroke();
+    fill(0);
+    ellipse(70, 70, 400, 400);
+    fill(150, 0, 0);
+    ellipse(70, 70, 300, 300);
+    fill(248, 233, 216);
+    ellipse(70, 70, 200, 200);
+
+    //circle two
+    fill(150, 0, 0);
+    ellipse(300, 300, 200, 200);
+    fill(199, 199, 199);
+    ellipse(300, 300, 100, 100);
+    fill(150);
+    ellipse(300, 300, 50, 50);
+
+    //circle three
+    fill(255);
+    ellipse(650, 170, 250, 250);
+    fill(199, 199, 199);
+    ellipse(650, 170, 200, 200);
+    fill(150, 0, 0);
+    ellipse(650, 170, 50, 50);
+
+    //circle five
+    stroke(255);
+    fill(0);
+    ellipse(250, 750, 300, 200);
+    ellipse(250, 750, 250, 150);
+    ellipse(250, 750, 200, 100);
+    ellipse(250, 750, 150, 50);
+
+    fill(255);
+    noStroke();
+    ellipse(100, 730, 100, 150);
+    fill(150, 0, 0);
+    noStroke();
+    ellipse(30, 800, 100, 100);
+
+    //circle three
+    fill(150,0,0);
+    noStroke();
+    ellipse(500, 700, 250, 250);
+    fill(0);
+    ellipse(600, 850, 200, 200);
+    fill(248, 233, 216);
+    ellipse(700, 900, 100, 600);
+
+}
+
+function draw() {
+
+}
